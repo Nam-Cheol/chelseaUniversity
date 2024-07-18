45a76856bf6f477bf5274ae93e690b35be32d12a
chorok917 <chorok917@gmail.com>
2024-07-18 20:53:17 +0900

    학사관리-학생명단조회 하는 중

--- src/main/java/com/chelseaUniversity/ver1/controller/TuitionController.java
+++ src/main/java/com/chelseaUniversity/ver1/controller/TuitionController.java
@@ -8,16 +8,18 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/tuition/*")
 public class TuitionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public TuitionController() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TuitionController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		// 인증처리
 		/*
@@ -25,37 +27,58 @@ public class TuitionController extends HttpServlet {
 		 * session.getAttribute("") == null) {
 		 * response.sendRedirect(request.getContextPath() + "/user/signin"); return; }
 		 */
-		
+
 		String action = request.getPathInfo();
 		switch (action) {
-		
-		// 교직원 -> 학사관리 -> 등록금 고지서 발송
+
+		// 교직원 -> 학사관리 -> 등록금 고지서 페이지
 		case "/bill":
 			request.getRequestDispatcher("/WEB-INF/views/tuition/createPayment.jsp").forward(request, response);
-//			showSendBillPage(request,response,session);
+			break;
+		case "/create":
+			request.getRequestDispatcher("/WEB-INF/views/user/studentList.jsp").forward(request, response);
 			break;
 
 		default:
 			response.sendError(HttpServletResponse.SC_NOT_FOUND);
 			break;
 		}
-	
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 인증처리
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute("") == null) {
+			response.sendRedirect(request.getContextPath() + "/user/signin");
+			return;
+		}
+
+		String action = request.getPathInfo();
+		switch (action) {
+
+		// 교직원 -> 학사관리 -> 등록금 고지서 발송하기
+		case "/create":
+			handleCreateBill(request, response, session);
+			break;
+
+		default:
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			break;
+		}
+
 	}
 
 	/**
-	 * 교직원 -> 학사관리 -> 등록금 고지서 발송 페이지
+	 * 교직원이 등록금 납부서 발송
 	 * @param request
 	 * @param response
 	 * @param session
 	 */
-	private void showSendBillPage(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
-		// TODO Auto-generated method stub
+	private void handleCreateBill(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
 		
-	}
-
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/tuition/createPayment.jsp
+++ src/main/webapp/WEB-INF/views/tuition/createPayment.jsp
@@ -2,6 +2,12 @@
     pageEncoding="UTF-8"%>
 <%@ include file="/WEB-INF/views/home/studentHeader.jsp" %>
 
+<section>
 <h1>등록금 고지서</h1>
+
+<form action="${pageContext.request.contextPath}/tuition/create" method="post">
+<button type="submit">등록금 고지서 발송</button>
+</form>
+</section>
 
 <%@ include file="/WEB-INF/views/home/footer.jsp" %>
