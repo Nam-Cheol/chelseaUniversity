f6dc0b17344357de6a15e4b4870d1bdf55763977
chorok917 <chorok917@gmail.com>
2024-07-18 18:17:02 +0900

    헤더 완성

--- src/main/java/com/chelseaUniversity/ver1/controller/StaffAcademicMgmt.java
+++ src/main/java/com/chelseaUniversity/ver1/controller/StaffAcademicMgmt.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet("/user/*")
+@WebServlet("/desk")
 public class StaffAcademicMgmt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
