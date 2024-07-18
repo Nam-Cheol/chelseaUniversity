420959d81e5bc05ba1aa4473e6447516c9cbb255
spglados <spglados@gmail.com>
2024-07-18 18:19:04 +0900

    asdasd

--- src/main/java/com/chelseaUniversity/ver1/controller/adminController.java
+++ src/main/java/com/chelseaUniversity/ver1/controller/adminController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 @WebServlet("/admin/*")
@@ -15,6 +17,24 @@ public class adminController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getPathInfo();
+
+		switch (action) {
+		case "/admin":
+			request.getRequestDispatcher("/WEB-INF/views/admin/admintest.jsp").forward(request, response);
+			break;
+		case "/delete":
+			
+			break;
+		case "/update":
+			
+			break;
+		default:
+			break;
+		}
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/webapp/WEB-INF/views/admin/admintest.jsp
+++ src/main/webapp/WEB-INF/views/admin/admintest.jsp
@@ -4,9 +4,19 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>어드민 테스트</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-
+	<h2>단과대학 등록</h2>
+	<form action="create-college" method="post">
+		<label for="title"> 단과이름 : </label>
+		<input type="text" id="title" name="title" value="ㅇㅇ대학">
+		<button type="submit">등록</button>
+		<button type="submit">삭제</button>
+	</form>
+	<br>
+	<a href="read-college">단과대학 목록</a>
+	
 </body>
 </html>
