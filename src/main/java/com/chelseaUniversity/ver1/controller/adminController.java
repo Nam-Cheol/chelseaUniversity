7e48d748ae78799feec232df4facdd2539cfed8c
spglados <spglados@gmail.com>
2024-07-22 12:12:47 +0900

    충돌해결

--- src/main/java/com/chelseaUniversity/ver1/controller/adminController.java
+++ src/main/java/com/chelseaUniversity/ver1/controller/adminController.java
@@ -1,13 +1,12 @@
 package com.chelseaUniversity.ver1.controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import java.io.IOException;
 
 @WebServlet("/admin/*")
 public class adminController extends HttpServlet {
