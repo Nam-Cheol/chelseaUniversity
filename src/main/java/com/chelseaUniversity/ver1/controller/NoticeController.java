66f7fe92ef33ae912cdc0b58c76eec81a97a491a
wonseok <misaka112@naver.com>
2024-07-24 21:04:50 +0900

    검색기능 완

--- src/main/java/com/chelseaUniversity/ver1/controller/NoticeController.java
+++ src/main/java/com/chelseaUniversity/ver1/controller/NoticeController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import com.chelseaUniversity.ver1.model.Notice;
+import com.chelseaUniversity.ver1.model.dto.NoticePageFormDto;
 import com.chelseaUniversity.ver1.repository.NoticeRepositoryImpl;
 import com.chelseaUniversity.ver1.repository.interfaces.NoticeRepository;
 
@@ -37,6 +38,7 @@ public class NoticeController extends HttpServlet {
 			break;
 		case "/detail":
 			showDetailPage(request,response);
+			break;
 		default:
 			break;
 		}
@@ -50,6 +52,8 @@ public class NoticeController extends HttpServlet {
 		int id = Integer.parseInt(request.getParameter("page"));
 		System.out.println(id);
 		Notice notice = noticeRepository.selectById(id);
+		String ipAddress=request.getRemoteAddr();
+		noticeRepository.updateViews(id);
 		request.setAttribute("notice", notice);
 		request.getRequestDispatcher("/WEB-INF/views/board/noticeDetail.jsp").forward(request, response);
 	}
@@ -65,6 +69,36 @@ public class NoticeController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+			String action = request.getPathInfo();
+		
+			switch (action) {
+			case "/search":
+				searchNotice(request,response);
+				break;
+			default:
+				break;
+			}
 	}
-
+			
+	/*
+	 * 검색 처리 기능
+	 */
+	private void searchNotice(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String type = request.getParameter("type");
+			String order = request.getParameter("order");
+				if(type != null && order != null) {
+					NoticePageFormDto notice = NoticePageFormDto.builder().keyword(order).build();
+					if(type.equals("title")) {
+						List<Notice>noticeList = noticeRepository.selectNoticeByTitle(notice);
+						request.setAttribute("noticeList", noticeList);
+						request.getRequestDispatcher("/WEB-INF/views/board/notice.jsp").forward(request, response);
+					} else{
+						List<Notice>noticeList = noticeRepository.selectNoticeByKeyword(notice);
+						request.setAttribute("noticeList", noticeList);
+						request.getRequestDispatcher("/WEB-INF/views/board/notice.jsp").forward(request, response);
+					}
+				} else {
+					
+				}
+			} 
 }
