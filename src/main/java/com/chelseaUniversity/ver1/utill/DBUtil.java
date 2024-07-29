692d9c60e34fc27c4a18340fb4f68cc943f08321
nambanote <namcher9428@gmail.com>
2024-07-29 10:04:29 +0900

    check

--- src/main/java/com/chelseaUniversity/ver1/utill/DBUtil.java
+++ src/main/java/com/chelseaUniversity/ver1/utill/DBUtil.java
@@ -11,12 +11,8 @@ public class DBUtil {
 	
 	private static DataSource dataSource;
 	
-	// 정적 초기화 블록 
 	static {
-		// TODO - 삭제 예정
-		System.out.println("11111111111111111");
 		try {
-			// InitialContext 객체를 생성하여 JNDI API 기술을 통해 존재하는 리소를 찾는 방법
 			InitialContext ctx = new InitialContext();
 			dataSource = (DataSource)ctx.lookup("java:comp/env/jdbc/university");
 		} catch (NamingException e) {
